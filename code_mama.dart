c23979527df8bbb2fa996165210495f975716f8e
Rohan Ahmed <148573695+justrealrohan@users.noreply.github.com>
2023-11-23 23:30:27 +0600

    Add files via upload

---
+++ code_mama.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+void main(){
+  int a = int.tryParse(stdin.readLineSync()?? ' ')?? 0;
+  int b = int.tryParse(stdin.readLineSync()?? ' ')?? 0;
+  int x = a+b;
+  print('x = $x');
+}
